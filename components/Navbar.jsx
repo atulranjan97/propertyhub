a590d87c49f4742690a969aad4847b879ae0fec2
atulranjan97 <atul.ranjan098@gmail.com>
2026-05-15 21:17:00 +0530

    Fix mobile login/Register button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -241,12 +241,18 @@ const Navbar = () => {
                 Add Property
               </Link>
             )}
-            {!session && (
-              <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
-                <i className="fa-brands fa-google mr-2"></i>
-                <span>Login or Register</span>
-              </button>
-            )}
+            {!session &&
+              providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  key={provider.id}
+                  onClick={() => signIn(provider.id)}
+                  className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5"
+                >
+                  <FaGoogle />
+                  <span className='ml-2'>Login or Register</span>
+                </button>
+              ))}
           </div>
         </div>
       )}
